d03f2a3e88f02d0952847959717551bee1b1e873
okanon <flat.cydia37jins.pc@gmail.com>
2017-08-26 00:39:08 +0900

    add Test/main.go

---
+++ Test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/okanon/linethrift.golang/LineThrift"
+)
+
+func main() {
+	thrift.THttpClient.THttpClient
+	LineThrift.TalkService.AcceptGroupInvitation
+}
